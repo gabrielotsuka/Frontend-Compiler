e2c304f539d0fedf725c91e9b2ff29667e587d10
gabrielotsuka <gabrielotsuka@gmail.com>
2023-06-03 02:12:49 -0300

    WIP Defines

---
+++ sintatic.cpp
@@ -0,0 +1,186 @@
+#include<stack>
+#include<vector>
+
+
+using namespace std;
+
+//Terminal
+#define FUNCTION 0	
+#define ABRE_CHAVES 1	
+#define TIPO 2	
+#define ID 3	
+#define SE 4	
+#define ENQUANTO 5	
+#define REPITA 6	
+#define VIRGULA 7	
+#define CONST_INT 8	
+#define CONST_CHAR 9	
+#define CONST_FLOAT 10
+#define ABRE_PARENTESES 11
+#define OP_ARIT_PREC3 12
+#define OP_ARIT_PREC2 13
+#define OP_ARIT_PREC1 14
+#define SENAO 15
+#define ATE 16
+#define FECHA_CHAVES 17
+#define PONTO_VIRGULA 18
+#define FECHA_PARENTESES 19
+#define RELOP 20
+#define FACA 21
+#define ENTAO 22
+
+//Not terminal
+#define INI 23 
+#define BLOCO 24 
+#define BLOCO_AUX 25 
+#define DECL_VARS 26 
+#define DECL_VARS_FAT 27 
+#define DECL_VAR 28 
+#define LISTA_ID 29 
+#define LISTA_ID_FAT 30 
+#define CMDS 31 
+#define CMDS_FAT 32 
+#define CMD 33 
+#define CMD_ATRIB 34 
+#define ARIT3 35 
+#define ARIT3_ 36 
+#define ARIT2 37 
+#define ARIT2_ 38 
+#define ARIT1 39 
+#define ARIT1_ 40 
+#define ARIT_FATOR 41 
+#define CMD_COND 42 
+#define COND 43 
+#define CMD_BLOCO 44 
+#define SENAO_FAT 45 
+#define CMD_REP 46 
+
+#
+
+typedef struct {
+    int tipo;
+    int valor;
+} Token;
+
+typedef struct {
+    int cabeça;
+    vector<int> Corpo; 
+} Producao;
+
+int predictiveTable[24][23];
+int** buildPredictiveTable() {
+    for (int i = 0; i < 24; i++) {
+        for(int j = 0; j < 23; j++) {
+            predictiveTable[i][j] = 0;
+        }
+    }
+    int offset = 23;
+    predictiveTable[INI-offset][FUNCTION] = 1;
+    predictiveTable[BLOCO-offset][ABRE_CHAVES] = 2;
+    predictiveTable[BLOCO_AUX-offset][TIPO] = 3;
+    predictiveTable[BLOCO_AUX-offset][ID] = 4;
+    predictiveTable[BLOCO_AUX-offset][SE] = 4;
+    predictiveTable[BLOCO_AUX-offset][ENQUANTO] = 4;
+    predictiveTable[BLOCO_AUX-offset][REPITA] = 4;
+    predictiveTable[DECL_VARS-offset][TIPO] = 5;
+    predictiveTable[DECL_VARS_FAT-offset][TIPO] = 6;
+    predictiveTable[DECL_VARS_FAT-offset][ID] = 7;
+    predictiveTable[DECL_VARS_FAT-offset][SE] = 7;
+    predictiveTable[DECL_VARS_FAT-offset][ENQUANTO] = 7;
+    predictiveTable[DECL_VARS_FAT-offset][REPITA] = 7;
+    predictiveTable[DECL_VAR-offset][TIPO] = 8;
+    predictiveTable[LISTA_ID-offset][ID] = 9;
+    predictiveTable[LISTA_ID_FAT-offset][VIRGULA] = 10;
+    predictiveTable[LISTA_ID_FAT-offset][PONTO_VIRGULA] = 11;
+    predictiveTable[CMDS-offset][ID] = 12;
+    predictiveTable[CMDS-offset][SE] = 12;
+    predictiveTable[CMDS-offset][ENQUANTO] = 12;
+    predictiveTable[CMDS-offset][REPITA] = 12;
+    predictiveTable[CMDS_FAT-offset][ID] = 13;
+    predictiveTable[CMDS_FAT-offset][SE] = 13;
+    predictiveTable[CMDS_FAT-offset][ENQUANTO] = 13;
+    predictiveTable[CMDS_FAT-offset][REPITA] = 13;
+    predictiveTable[CMDS_FAT-offset][FECHA_CHAVES] = 14;
+    predictiveTable[CMD-offset][ID] = 15;
+    predictiveTable[CMD-offset][SE] = 16;
+    predictiveTable[CMD-offset][ENQUANTO] = 17;
+    predictiveTable[CMD-offset][REPITA] = 17;
+    predictiveTable[CMD_ATRIB-offset][ID] = 18;
+    predictiveTable[ARIT3-offset][ID] = 19;
+    predictiveTable[ARIT3-offset][CONST_INT] = 19;
+    predictiveTable[ARIT3-offset][CONST_CHAR] = 19;
+    predictiveTable[ARIT3-offset][CONST_FLOAT] = 19;
+    predictiveTable[ARIT3-offset][ABRE_PARENTESES] = 19;
+    predictiveTable[ARIT3_-offset][OP_ARIT_PREC3] = 20;
+    predictiveTable[ARIT3_-offset][RELOP] = 21;
+    predictiveTable[ARIT3_-offset][PONTO_VIRGULA] = 21;
+    predictiveTable[ARIT3_-offset][FECHA_PARENTESES] = 21;
+    predictiveTable[ARIT2-offset][ID] = 22;
+    predictiveTable[ARIT2-offset][CONST_INT] = 22;
+    predictiveTable[ARIT2-offset][CONST_CHAR] = 22;
+    predictiveTable[ARIT2-offset][CONST_FLOAT] = 22;
+    predictiveTable[ARIT2-offset][ABRE_PARENTESES] = 22;
+    predictiveTable[ARIT2_-offset][OP_ARIT_PREC3] = 24;
+    predictiveTable[ARIT2_-offset][OP_ARIT_PREC2] = 23;
+    predictiveTable[ARIT2_-offset][RELOP] = 24;
+    predictiveTable[ARIT2_-offset][PONTO_VIRGULA] = 24;
+    predictiveTable[ARIT2_-offset][FECHA_PARENTESES] = 24;
+    predictiveTable[ARIT1-offset][ID] = 25;
+    predictiveTable[ARIT1-offset][CONST_INT] = 25;
+    predictiveTable[ARIT1-offset][CONST_CHAR] = 25;
+    predictiveTable[ARIT1-offset][CONST_FLOAT] = 25;
+    predictiveTable[ARIT1-offset][ABRE_PARENTESES] = 25;
+    predictiveTable[ARIT1_-offset][OP_ARIT_PREC3] = 27;
+    predictiveTable[ARIT1_-offset][OP_ARIT_PREC2] = 27;
+    predictiveTable[ARIT1_-offset][OP_ARIT_PREC1] = 26;
+    predictiveTable[ARIT1_-offset][RELOP] = 27;
+    predictiveTable[ARIT1_-offset][PONTO_VIRGULA] = 27;
+    predictiveTable[ARIT1_-offset][FECHA_PARENTESES] = 27;
+    predictiveTable[ARIT_FATOR-offset][ID] = 28;
+    predictiveTable[ARIT_FATOR-offset][CONST_INT] = 29;
+    predictiveTable[ARIT_FATOR-offset][CONST_CHAR] = 30;
+    predictiveTable[ARIT_FATOR-offset][CONST_FLOAT] = 40;
+    predictiveTable[ARIT_FATOR-offset][ABRE_PARENTESES] = 31;
+    predictiveTable[CMD_COND-offset][SE] = 32;
+    predictiveTable[COND-offset][ID] = 33;
+    predictiveTable[COND-offset][CONST_INT] = 33;
+    predictiveTable[COND-offset][CONST_CHAR] = 33;
+    predictiveTable[COND-offset][CONST_FLOAT] = 33;
+    predictiveTable[COND-offset][ABRE_PARENTESES] = 33;
+    predictiveTable[CMD_BLOCO-offset][ABRE_CHAVES] = 35;
+    predictiveTable[CMD_BLOCO-offset][ID] = 34;
+    predictiveTable[CMD_BLOCO-offset][SE] = 34;
+    predictiveTable[CMD_BLOCO-offset][ENQUANTO] = 34;
+    predictiveTable[CMD_BLOCO-offset][REPITA] = 34;
+    predictiveTable[SENAO_FAT-offset][ABRE_CHAVES] = 37;
+    predictiveTable[SENAO_FAT-offset][ID] = 37;
+    predictiveTable[SENAO_FAT-offset][SE] = 37;
+    predictiveTable[SENAO_FAT-offset][ENQUANTO] = 37;
+    predictiveTable[SENAO_FAT-offset][REPITA] = 37;
+    predictiveTable[SENAO_FAT-offset][SENAO] = 36;
+    predictiveTable[SENAO_FAT-offset][ATE] = 37;
+    predictiveTable[CMD_REP-offset][ENQUANTO] = 38;
+    predictiveTable[CMD_REP-offset][REPITA] = 39;
+}
+
+
+int main() {
+    stack<int> stack;
+    stack.push(INI);
+    Token proxToken;//lex();
+
+    while(!stack.empty()) {
+        int expectedSymbol = stack.top();
+        if(expectedSymbol < 23) {
+            if (expectedSymbol == proxToken.tipo) {
+                stack.pop();
+                proxToken;//lex();
+            } else {
+                // throw error
+            }
+        } else {
+            // DERIVA
+        }
+    }
+    return 0;
+}
